b4212edd3ae411ce543f1285f7f846593da36a25
koukai76 <61773788+koukai76@users.noreply.github.com>
2022-09-30 15:56:17 +0900

    Update app2.js

--- app2.js
+++ app2.js
@@ -2,9 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const moment = require('moment-timezone');
-// require('moment/locale/ja');
 
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 1234;
@@ -27,4 +26,5 @@ app.listen(port, async () => {
   const now = moment(new Date());
   now.tz('Asia/Tokyo').format('ha z');
   console.log(now.format('YYYY-MM-DD-HH'));
+  console.log(1);
 });
